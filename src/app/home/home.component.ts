a31f493ae354f9d64578761bf6029bb5acc042e3
han <hanhm1997@gmail.com>
2023-02-14 16:33:57 +0700

    data blinding property

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  public name = 'Huy';
+  public name = 'Hân Hà';
 
   constructor() {
   }
