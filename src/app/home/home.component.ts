2d18018ec6671ed396eb28c119789a5d61a1f734
han <hanhm1997@gmail.com>
2023-02-15 16:47:12 +0700

    ngfor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,9 +8,15 @@ import {Component, OnInit} from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = 'Hân Hà';
   public age = 15;
-
+  public fruits = ["TAO", "CAM", "OI", "NHO"];
+  public fruit2 = [
+    {ten:'Táo', gia: 4000},
+    {ten:'Nho', gia:  3000},
+    {ten:'Cam', gia:  5000},
+  ];
   constructor() {
   }
   ngOnInit():void {
+    console.log("fruits"+ this.fruits);
   }
 }
